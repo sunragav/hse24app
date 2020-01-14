9068cba1cf252f6d4e757df8e9b81e8626bbbb5f
Sundara Raghavan <sunragav@gmail.com>
2020-01-14 02:53:42 +0100

    Making the remote service call run in the IO thread pool

--- data/src/main/java/com/sunragav/data/CatalogRepositoryImpl.kt
+++ data/src/main/java/com/sunragav/data/CatalogRepositoryImpl.kt
@@ -3,11 +3,14 @@ package com.sunragav.data
 import com.sunragav.data.remote.RemoteCatalogSource
 import com.sunragav.domain.models.DomainCatalog
 import com.sunragav.domain.repository.CatalogRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CatalogRepositoryImpl @Inject constructor(private val remoteRepository: RemoteCatalogSource) :
     CatalogRepository {
-    override suspend fun getCatalog(): List<DomainCatalog> {
-        return remoteRepository.getCatalog()
+    override suspend fun getCatalog(): List<DomainCatalog> = withContext(Dispatchers.IO) {
+        remoteRepository.getCatalog()
     }
 }
